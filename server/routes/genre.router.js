a3604e1b428b10238698eafc679020ca0bf336bb
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-03-04 23:12:01 -0600

    when a movie is clicked, this will retrieve the genres related to that movie and then send them to the client

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -4,9 +4,24 @@ const pool = require('../modules/pool')
 
 router.get('/:id', (req, res) => {
   // Add query to get all genres
-console.log('the id of movie clicked', req.params.id);
+  console.log('the id of movie clicked', req.params.id);
+  // variable to old id from get request parameters
+  const movieId = req.params.id
 
-  res.sendStatus(418)
+  // SQL command to get the genres of the movie clicked from the db
+  const sqlText = `
+  SELECT "genres".name AS genre FROM "movies"
+  JOIN "movies_genres" on "movies".id = "movies_genres".movie_id
+  JOIN "genres" on "genres".id = "movies_genres".genre_id
+  WHERE "movies".id = ${movieId};`
+
+  pool.query(sqlText)
+    .then(results => {
+      console.log('genres have been fetched');
+      res.send(results.rows)
+    }).catch(err => {
+      res.sendStatus(500)
+    })
 });
 
 module.exports = router;
